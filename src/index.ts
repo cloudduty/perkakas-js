2806e1e27c3c50d48ccb54db904bd626af34857a
haryfun <hary.vai@gmail.com>
2021-04-29 02:41:59 +0700

    feat: add index file for export all of modules

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { convertCurrency } from './convertCurrency';
+import { formatDate } from './formatDate';
+import { formatTime } from './formatTime';
+import { titleCase, isSpecialChar, replaceSpecialChar } from './titleCase';
+
+export {
+  convertCurrency,
+  formatDate,
+  formatTime,
+  titleCase,
+  isSpecialChar,
+  replaceSpecialChar
+}
